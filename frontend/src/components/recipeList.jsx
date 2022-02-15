77db2140b8a5119bb8b5b78f99c112cd86d4021f
ZanderKoch <89077492+ZanderKoch@users.noreply.github.com>
2022-02-15 14:36:42 +0100

    added component RecepieList

---
+++ frontend/src/components/recipeList.jsx
@@ -0,0 +1,21 @@
+/**
+ * @file an element that displays a list of all recepies
+ * @author Zander Koch
+ */
+
+function RecipeList(){
+    //sätt ett dummy-objekt av en lista med två recept-enteties här
+    let testRecipes
+
+    return(
+        <section className="recipe-list">
+            <header>
+                <h3>
+                    Recept:
+                </h3>
+            </header>
+        </section>
+    );
+}
+
+export default RecipeList;
