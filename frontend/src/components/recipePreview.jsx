c518914a11150e32680da8e70b089c777b209da6
ZanderKoch <89077492+ZanderKoch@users.noreply.github.com>
2022-02-16 13:29:00 +0100

    implemented recipe preview component

---
+++ frontend/src/components/recipePreview.jsx
@@ -0,0 +1,23 @@
+/**
+ * @file an element that displays a preview of provided recipe
+ * @author Zander Koch
+ * @todo implement
+ */
+
+//import "./component styles/recipePreview.scss";
+
+function RecipePreview(props){
+    return(
+        <article>
+            <header>
+                <h4>{props.title}</h4>
+            </header>
+            <span>&#9733;{props.starCount}</span>
+            <p>
+                {props.description}
+            </p>
+        </article>
+    );
+}
+
+export default RecipePreview;
