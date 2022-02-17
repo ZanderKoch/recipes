b4466616da654aed94a1797e420e9925f0b2b0a5
ZanderKoch <89077492+ZanderKoch@users.noreply.github.com>
2022-02-17 14:58:59 +0100

    Implemented ConnectionFactory.getConnection()

--- recipes/src/main/java/recipes/ConnectionFactory.java
+++ recipes/src/main/java/recipes/ConnectionFactory.java
@@ -1,14 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package recipes;
 
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
- * creates data
+ * class for getting connections to the database
  * @author Zander Koch
  */
 public class ConnectionFactory{
-    
+    public static Connection getConnection()
+            throws ClassNotFoundException, SQLException{
+        
+        System.out.println("ConnectionFactory.getConnection(): "
+                + "attempting to get database connection");
+        
+        String user = Util.getProperty(PropKey.DB_USER);
+        String password = Util.getProperty(PropKey.DB_PASSWORD);
+        String url = Util.getProperty(PropKey.DB_URL);
+        
+        return (Connection) DriverManager.getConnection(url, user, password);
+    }
 }
