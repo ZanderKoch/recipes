48b4534e0e7d28a352edf61ab346f9b8945afe44
ZanderKoch <89077492+ZanderKoch@users.noreply.github.com>
2022-02-28 18:58:30 +0100

    end of day comitt

--- recipes/src/main/java/beans/RecipeBean.java
+++ recipes/src/main/java/beans/RecipeBean.java
@@ -1,9 +1,11 @@
 package beans;
 
 import com.mysql.jdbc.Connection;
+import entities.Recipe;
 import entities.ReturnSprout;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import javax.ejb.Stateless;
 import javax.ws.rs.core.Response;
 import recipes.ConnectionFactory;
@@ -22,7 +24,7 @@ public class RecipeBean{
      */
     public ReturnSprout getRecipes(){
         try(Connection connection = ConnectionFactory.getConnection()){
-            ArrayList<Recipe>
+            ArrayList<Recipe> recipes = new ArrayList();
             
             String sql = "SELECT * FROM recipe";
             PreparedStatement statement = connection.prepareStatement(sql);
@@ -30,12 +32,22 @@ public class RecipeBean{
             
             if(!result.first()){
                 return new ReturnSprout("no recipes found"
-                        ,Response.Status.CREATED);
+                        ,Response.Status.NOT_FOUND);
             }
-            while(result.)
+            
         }
         catch(Exception e){
             System.out.println("RecipeBean.getRecipes():" + e);
         }
+    }
+    
+    /**
+     * returns an arraylist of recepies with the basic attributes that can be
+     * gotten from 
+     * @param resultSet
+     * @return an arraylist of recipes with id, author, title, and description
+     */
+    private static ArrayList<Recipe> getBasicRecipeList(ResultSet resultSet){
+        
     }
 }
--- recipes/src/main/java/entities/Recipe.java
+++ recipes/src/main/java/entities/Recipe.java
@@ -16,4 +16,84 @@ public class Recipe{
     private ArrayList<String> instructions;
     private int starCount;
     private ArrayList<Comment> comments;
+    
+    public Recipe(){
+        
+    }
+    
+    /**
+     * constructor for creating recipe with all atributes found in recipe table
+     */
+    public Recipe(int id, String author, String title, String description){
+        this.id = id;
+        this.author = author;
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public ArrayList<String> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(ArrayList<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ArrayList<String> getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(ArrayList<String> instructions) {
+        this.instructions = instructions;
+    }
+
+    public int getStarCount() {
+        return starCount;
+    }
+
+    public void setStarCount(int starCount) {
+        this.starCount = starCount;
+    }
+
+    public ArrayList<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(ArrayList<Comment> comments) {
+        this.comments = comments;
+    }
+    
+    
+    
 }
