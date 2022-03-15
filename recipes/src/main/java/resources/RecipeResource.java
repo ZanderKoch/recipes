571d1bf6e9c55e6dfe727bd4e97ce71b147951f6
ZanderKoch <89077492+ZanderKoch@users.noreply.github.com>
2022-03-15 12:24:29 +0100

    added resource addrecipe

--- recipes/src/main/java/resources/RecipeResource.java
+++ recipes/src/main/java/resources/RecipeResource.java
@@ -1,10 +1,14 @@
 package resources;
 
 import beans.RecipeBean;
+import entities.Recipe;
 import entities.ReturnSprout;
 import javax.ejb.EJB;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 /**
@@ -22,5 +26,11 @@ public class RecipeResource{
         return Response.status(sprout.getStatus())
                 .entity(sprout.getEntity())
                 .build();
+    }
+    
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response addRecipe(Recipe recipe){
+        ReturnSprout sprout = recipeBean.addRecipe(recipe);
     }
 }
